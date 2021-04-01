5bddbe7873fc8c6882fea63febff88b827e7c411
boat.panitarn <boat.panitarn@bitkub.com>
2021-04-01 19:04:14 +0700

    :hammer: edit res

--- services/market.js
+++ services/market.js
@@ -1,29 +1,30 @@
-const axios = require('axios')
-const { curl } = require('../helper/curl')
+const axios = require("axios");
+const { curl } = require("../helper/curl");
 
 const bdo = async () => {
-    const url = `https://api.bdollar.fi/api/bdollar/get-token-info?token=BDO`
-    const res = await curl(`curl ${url}`)
-    console.log(res)
-    let data = JSON.parse(res)
-    if(!data){
-        await bdo()
-    }
-    return data
-}
+  const url = `https://api.bdollar.fi/api/bdollar/get-token-info?token=BDO`;
+  try {
+    const res = await curl(`curl ${url}`);
+    console.log(JSON.parse(res));
+    return JSON.parse(res);
+  } catch (err) {
+    return await bdo();
+  }
+};
 
 const sBdo = async () => {
-    const url = `https://api.bdollar.fi/api/bdollar/get-token-info?token=sBDO`
-    const res = await curl(`curl ${url}`)
-    console.log(res)
-    let data = JSON.parse(res)
-    if(!data){
-        await sBdo()
-    }
-    return data
-}
+  const url = `https://api.bdollar.fi/api/bdollar/get-token-info?token=sBDO`;
+  const res = await curl(`curl ${url}`);
+  try {
+    const res = await curl(`curl ${url}`);
+    console.log(JSON.parse(res));
+    return JSON.parse(res);
+  } catch (err) {
+    return await bdo();
+  }
+};
 
 module.exports = {
-    bdo,
-    sBdo
-}
+  bdo,
+  sBdo,
+};
